828fd782330f22bed7337fe6762dd8d97cda8a92
Fran Jimenez <frjim26@gmail.com>
2022-05-20 13:24:47 +0200

    form validate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,    
+    MatCardModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,27 @@
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss']
+})
+export class LoginFormComponent {
+  addressForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(5)]],   
+    record: [false]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {     
+    if (this.addressForm.valid) { 
+      alert('Usuario logueado con éxito')
+      if (this.addressForm.controls.record.value) {
+        localStorage.setItem('user', JSON.stringify(this.addressForm.value));      
+      }
+    }
+  }
+}
